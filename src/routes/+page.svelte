7c990dd89e8da2336643e71eb8f2a31bc4a86e32
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-09-24 17:40:19 +0000

    Install ShadCN, BitsUI and Melt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Test Page</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/vendor/shadcn/components/ui/button';
+</script>
+
+<main class="block space-y-4 m-2">
+	<h1 class="text-4xl">ShadCN Test</h1>
+	<Button>Click me</Button>
+</main>
