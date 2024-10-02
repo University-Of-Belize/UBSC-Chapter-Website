ce4a33740a8a89ea308aac26b33705d44afe3e80
Immanuel Daviel A. Garcia <34188635+AlexDev404@users.noreply.github.com>
2024-10-02 17:42:01 +0000

    Finish creating `PublicNav`

---
+++ src/lib/components/public/nav.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Button } from '$lib/vendor/shadcn/components/ui/button';
+</script>
+
+<main class="flex text-slate-50 bg-sky-950 justify-between items-center px-20 py-2">
+	<div class="nav-logo flex items-center gap-1">
+		<img src="/landing/stock/acm_chapter_light.png" alt="ACM Chapter Logo" width="40px" />
+		<div class="font-thin text-lg">UBACM</div>
+	</div>
+	<div class="nav-links text-sm w-fit uppercase flex items-center gap-4 text-md font-light">
+		<div>ABOUT US</div>
+		<div>GROUPS</div>
+		<div>COMMITEES</div>
+		<div>PROJECTS</div>
+		<div>CONTACT</div>
+	</div>
+	<Button class="bg-sky-500 hover:bg-sky-600">Login with Email</Button>
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { Button } from '$lib/vendor/shadcn/components/ui/button';
+	import PublicNav from '$lib/components/public/nav.svelte'
 </script>
 
+<PublicNav/>
 <main class="block space-y-4 m-2">
 	<h1 class="text-4xl">ShadCN Test</h1>
 	<Button>Click me</Button>
